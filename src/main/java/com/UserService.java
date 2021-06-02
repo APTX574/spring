e6ec3b6fba0da52386e70be3cbad7b9c967a3bfc
574 <2659332353@qq.com>
2021-06-02 10:02:37 +0800

    1.0

--- src/main/java/com/UserService.java
+++ src/main/java/com/UserService.java
@@ -6,23 +6,28 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+
 public class UserService {
     @Autowired
     private UserDaoImpl userDao;
 
 
-
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
+
+        userDao.addUser(new User(user.getName() + "as", user.getId() + 1, user.getPrice() + 123));
     }
 
     @Test
     public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         UserService userService = context.getBean("userService", UserService.class);
-        userService.addUser(new User("wqs",1,1212));
+        userService.addUser(new User("wqs",12,1212));
+        userService.addUser(new User("wqs1",11,12112));
 
     }
 }
